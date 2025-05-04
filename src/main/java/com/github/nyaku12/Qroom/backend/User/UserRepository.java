a644f5284ab4cb176a64aa73de2f0cc6ff547b18
nyaku12 <sukablyatmp4@gmail.com>
2025-05-04 16:59:56 +0500

    фулл работает добаваление/удаление юзера

--- src/main/java/com/github/nyaku12/Qroom/backend/User/UserRepository.java
+++ src/main/java/com/github/nyaku12/Qroom/backend/User/UserRepository.java
@@ -1,11 +1,7 @@
 package com.github.nyaku12.Qroom.backend.User;
 
-import com.github.nyaku12.Qroom.backend.User.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
